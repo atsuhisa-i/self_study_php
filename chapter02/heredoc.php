797da0885d5d0ed6f03ecd8c5b607035b9f45669
DESKTOP-D21AOUO\a-iino <a-iino@orbitalnet.jp>
2021-04-06 15:58:40 +0900

    定数とデータ型

---
+++ chapter02/heredoc.php
@@ -0,0 +1,10 @@
+<?php
+    $str = 'PHP (PHP:Hypertext Preprocessor)';
+    $msg = <<< EOD
+    {$str}は、サーバサイドで動作する簡易な
+    スクリプト言語です。
+    まずは、本書でじっくり基礎固めしましょう。\n
+    "Let's start, everyone!!"
+    EOD;
+
+    print $msg;
