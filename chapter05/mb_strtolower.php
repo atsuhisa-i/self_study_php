028957cb73423d40c0804e4e77aa3f1a3de6a5b7
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-04-24 20:36:32 +0900

    組み込み関数1

---
+++ chapter05/mb_strtolower.php
@@ -0,0 +1,4 @@
+<?php
+  mb_internal_encoding('UTF-8');
+  print strtolower('WINGSプロジェクト');
+  print mb_strtolower('WINGSプロジェクト');
---
+++ chapter05/str_replace1.php
@@ -0,0 +1,4 @@
+<?php
+  $str = 'にわにはにわにわとりがいる';
+  print str_replace('にわ', 'ニワ', $str, $cnt). "\n";
+  print "{$cnt}個の置き換えをしました。";
